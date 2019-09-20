2b166556b30135bf772222e58fd9bfbca7a4e857
mleeriis <mlee@riis.com>
2019-09-20 15:48:55 -0400

    Delete Balance is not required

--- src/main/java/com/riis/ui/controller/BalanceController.java
+++ src/main/java/com/riis/ui/controller/BalanceController.java
@@ -1,7 +1,6 @@
 package com.riis.ui.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,10 +42,4 @@ public class BalanceController {
 		
 		return returnValue;
 	}
-	
-	@DeleteMapping
-	public String deleteBalance() {
-		return "Delete balance";
-	}
-
 }
